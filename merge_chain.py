eb04c40fc478ae43d88b012ac72b38d4b3ea5f6a
Scott Christley <schristley@utsouthwestern.edu>
2025-04-18 19:39:03 +0000

    merge receptors

--- merge_chain.py
+++ merge_chain.py
@@ -38,6 +38,7 @@ ipa_cache_list = [
 cache_list = []
 cache_list.extend(ipa_cache_list)
 cache_list.extend(vdjserver_cache_list)
+#cache_list = ['7245411507393139181-242ac11b-0001-012']
 
 container = AIRRKnowledgeCommons()
 
@@ -68,6 +69,45 @@ for study in cache_list:
     print(len(container.chains))
     print(dup_cnt)
 
+    receptor_file = f'{directory_name}/ab_tcell_receptors.jsonl'
+    print(receptor_file)
+    with open(receptor_file, 'r') as f:
+        for line in f:
+            x = json.loads(line)
+            y = json_loader.load_any(x['ab_tcell_receptors'], AlphaBetaTCR)
+            #print(y)
+            if container.ab_tcell_receptors.get(y.akc_id) is None:
+                container.ab_tcell_receptors[y.akc_id] = y
+            else:
+                dup_cnt += 1
+    print(len(container.ab_tcell_receptors))
+
+    receptor_file = f'{directory_name}/gd_tcell_receptors.jsonl'
+    print(receptor_file)
+    with open(receptor_file, 'r') as f:
+        for line in f:
+            x = json.loads(line)
+            y = json_loader.load_any(x['gd_tcell_receptors'], GammaDeltaTCR)
+            #print(y)
+            if container.gd_tcell_receptors.get(y.akc_id) is None:
+                container.gd_tcell_receptors[y.akc_id] = y
+            else:
+                dup_cnt += 1
+    print(len(container.gd_tcell_receptors))
+
+    receptor_file = f'{directory_name}/bcell_receptors.jsonl'
+    print(receptor_file)
+    with open(receptor_file, 'r') as f:
+        for line in f:
+            x = json.loads(line)
+            y = json_loader.load_any(x['bcell_receptors'], BCellReceptor)
+            #print(y)
+            if container.bcell_receptors.get(y.akc_id) is None:
+                container.bcell_receptors[y.akc_id] = y
+            else:
+                dup_cnt += 1
+    print(len(container.bcell_receptors))
+
 # IEDB data
 directory_name = f'{iedb_data_dir}/iedb_jsonl'
 chain_file = f'{directory_name}/Chain.jsonl'
@@ -83,14 +123,39 @@ with open(chain_file, 'r') as f:
             dup_cnt += 1
 
 print(len(container.chains))
-print(dup_cnt)
 
+receptor_file = f'{directory_name}/AlphaBetaTCR.jsonl'
+print(receptor_file)
+with open(receptor_file, 'r') as f:
+    for line in f:
+        x = json.loads(line)
+        y = json_loader.load_any(x['ab_tcell_receptors'], AlphaBetaTCR)
+        #print(y)
+        if container.ab_tcell_receptors.get(y.akc_id) is None:
+            container.ab_tcell_receptors[y.akc_id] = y
+        else:
+            dup_cnt += 1
+
+receptor_file = f'{directory_name}/GammaDeltaTCR.jsonl'
+print(receptor_file)
+with open(receptor_file, 'r') as f:
+    for line in f:
+        x = json.loads(line)
+        y = json_loader.load_any(x['gd_tcell_receptors'], GammaDeltaTCR)
+        #print(y)
+        if container.gd_tcell_receptors.get(y.akc_id) is None:
+            container.gd_tcell_receptors[y.akc_id] = y
+        else:
+            dup_cnt += 1
 
 # Write everything to JSONL
-write_jsonl(container, 'chains', f'{ak_load_dir}/chains.jsonl')
+write_jsonl(container, 'chains', f'{ak_load_dir}/Chain.jsonl')
+write_jsonl(container, 'ab_tcell_receptors', f'{ak_load_dir}/AlphaBetaTCR.jsonl')
+write_jsonl(container, 'gd_tcell_receptors', f'{ak_load_dir}/GammaDeltaTCR.jsonl')
+write_jsonl(container, 'bcell_receptors', f'{ak_load_dir}/BCellReceptor.jsonl')
 
 # Write everything to CSV
-container_slot = ak_schema_view.get_slot('chains')
-tname = container_slot.range
-fname = tname + '.csv'
 write_csv(container, 'chains', f'{ak_load_dir}/Chain.csv')
+write_csv(container, 'ab_tcell_receptors', f'{ak_load_dir}/AlphaBetaTCR.csv')
+write_csv(container, 'gd_tcell_receptors', f'{ak_load_dir}/GammaDeltaTCR.csv')
+write_csv(container, 'bcell_receptors', f'{ak_load_dir}/BCellReceptor.csv')
