18c3125bb60f048d46df2e3d1842563eab2d41fd
Scott Christley <schristley@utsouthwestern.edu>
2026-03-23 17:59:27 +0000

    ignore duplicates

--- query_assay_load.sh
+++ query_assay_load.sh
@@ -11,5 +11,5 @@ docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 
 
 docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 psql ${PG_AK_CONN} -c "CREATE TABLE tmp_table (id SERIAL PRIMARY KEY, json_data JSONB);"
 docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 psql ${PG_AK_CONN} -c "\copy tmp_table (json_data) FROM '/ak_data/QueryAssay.jsonl' (FORMAT TEXT);"
-docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 psql ${PG_AK_CONN} -c "INSERT into "\"QueryAssay"\" SELECT json_data->>'akc_id', json_data FROM tmp_table;"
+docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 psql ${PG_AK_CONN} -c "INSERT into "\"QueryAssay"\" SELECT json_data->>'akc_id', json_data FROM tmp_table ON CONFLICT DO NOTHING;"
 docker run -v ${AIRRKB_IMPORT}:/ak_data --network ak-db-network -it postgres:16 psql ${PG_AK_CONN} -c "DROP TABLE tmp_table;"
